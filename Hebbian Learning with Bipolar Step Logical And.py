675f03580122a7e414e7b1531204fa31ecf162cb
MdRehanAli <rehanali21d>
2023-10-08 11:35:05 +0600

    Hebbian And Predicted and Graph Plotting.

--- Hebbian Learning with Bipolar Step Logical And.py
+++ Hebbian Learning with Bipolar Step Logical And.py
@@ -8,11 +8,36 @@ def Bipolar_Step(x):
 # Training
 x = np.array([[-1, -1, 1, 1], [-1, 1, -1, 1], [1, 1, 1, 1]])
 y = np.array([[-1, -1, -1, 1]])
-w = [0, 0, 0]
+w = np.array([[0, 0, 0]])
 
 for i in range(x.shape[1]):
     sample = x[:, i]
     w[0] = w[0] + y[0][i] * sample
 
     print(w)
+
+# Testing
+predicted = []
+for i in range(x.shape[1]):
+    sample = x[:, i]
+    y_ = np.dot(sample, w[0])
+    predicted.append(Bipolar_Step(y_))
+    print(predicted)
+
+# Decision Boundary
+dx = []
+dy = []
+for i in range(-2, 4):
+    dx.append(i)
+    dy.append((-w[0][0]/w[0][1])*i + (-w[0][2])/w[0][1])
+
+plt.scatter([-1, -1, 1], [-1, 1, -1])
+plt.scatter(1, 1)
+plt.plot(dx, dy, label="Decision Boundary")
+for i_x, i_y in zip([-1, -1, 1, 1], [-1, 1, -1, 1]):
+    plt.text(i_x, i_y, '({},{})'.format(i_x, i_y))
+plt.title("Habbian Learning And problem with Bipolar Step Function")
+plt.legend()
+plt.grid()
+plt.show()
 
